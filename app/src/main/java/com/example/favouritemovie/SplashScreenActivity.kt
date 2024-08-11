cce3e23b133d3b793cdbbd80555033a3e261849d
Prangula <Prangula731@gmail.com>
2024-08-11 17:46:26 +0400

    SplashScreen

---
+++ app/src/main/java/com/example/favouritemovie/SplashScreenActivity.kt
@@ -0,0 +1,21 @@
+package com.example.favouritemovie
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+
+@SuppressLint("CustomSplashScreen")
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+
+    }
+}
